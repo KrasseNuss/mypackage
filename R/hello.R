00d6fb40c2dcd2df17e80626a12182fbb3bacbce
Xaver Köppl <x.koeppl@campus.lmu.de>
2025-07-14 12:11:31 +0200

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,22 @@
+# You can learn more about package authoring with RStudio at:
+#
+#   https://r-pkgs.org
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+#
+#@param a Erste Zahl
+#@param b Zweite Zahl
+#@return Summe
+#@export
+
+addiere <- function(a, b) {
+  a+b
+}
+
+subtrahiere <- function(a, b){
+  a-b
+}
